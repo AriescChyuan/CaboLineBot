3e932e6cd57a09fc1acb2b568a09fe83a4860174
Bruce <jetliayu@gmail.com>
2021-09-08 15:32:27 +0800

    ok

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import os
 
 app = Flask(__name__)
 
@@ -43,4 +44,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run( port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
