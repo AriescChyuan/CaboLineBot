369b067db508690753f77c1b3e2de30efe895f52
Bruce <jetliayu@gmail.com>
2021-09-08 15:42:53 +0800

    ok

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 def handle_message(event):
     print('event= ', event)
     print('event.message= ',event.message)
-    if event.message.text == '1+1=':
+    if event.message.text == '1+1=  ':
 
         line_bot_api.reply_message(
             event.reply_token,
