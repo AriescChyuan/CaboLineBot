207f35f73521a16f33112bac9fffbc1e410e0bf1
Bruce <jetliayu@gmail.com>
2021-09-10 15:37:18 +0800

    ok

--- app.py
+++ app.py
@@ -119,17 +119,17 @@ def handle_message(event):
             movie_ranking += link+'\n'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=movie_ranking))
     elif msg == "新聞":
-        url = 'https://news.google.com/topstories?hl=zh-TW&gl=TW&ceid=TW:zh-Hant'
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser') 
-        x = soup.select('a.VDXfz')
-        news ='今日前五大新聞:\n'
-        for index, data in enumerate(x):
-            if index==5:
+       url = 'https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVlJYR2dKVVZ5Z0FQAQ?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
+       res = requests.get(url)
+       soup = BeautifulSoup(res.text, 'html.parser') 
+       x = soup.select('article.MQsxIb.xTewfe.R7GTQ.keNKEd.j7vNaf.Cc0Z5d.EjqUne a.VDXfz')
+       news =''
+       for index, data in enumerate(x):
+           if index==5:
                break
-            url = data['href']
-            news += "https://news.google.com/{}\n".format(url)
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=news))
+           url = data['href']
+           news += "https://news.google.com/{}\n".format(url)
+       line_bot_api.reply_message(event.reply_token,TextSendMessage(text=news))
 
     else:        
         pass    
