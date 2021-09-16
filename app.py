eed3f2e50c1e942b791baaff51068588de7dd0cf
Bruce <jetliayu@gmail.com>
2021-09-16 21:27:22 +0800

    ok

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.chrome.options import Options
 import time
 
 app = Flask(__name__)
@@ -65,7 +65,13 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=response))
 
     if msg.lower().find('#') == 0:
-        chrome = webdriver.Chrome('./chromedriver')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless") #無頭模式
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        # chrome = webdriver.Chrome('./chromedriver')
         target = msg
         url_list=[]
         try:
