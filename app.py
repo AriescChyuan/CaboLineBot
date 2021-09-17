c0e1e4d0ce945b03aee2f79f1981368a0eb54abe
Bruce <jetliayu@gmail.com>
2021-09-17 13:18:14 +0800

    ok

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def handle_message(event):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless") #無頭模式
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--no-sandbox")
         chrome = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         # chrome = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
         print('chrome: ',chrome)
