4d65bfab5269bb2a7e7c89eaef674d42ca1878a3
Bruce <jetliayu@gmail.com>
2021-09-22 12:54:56 +0800

    ok

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def handle_message(event):
         url_list=[]
         try:
             chrome.get("https://www.instagram.com/")
-            username = WebDriverWait(chrome, 5).until(
+            username = WebDriverWait(chrome, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'))
             )
             print('username:',username)
@@ -89,10 +89,10 @@ def handle_message(event):
             )
             password.send_keys('Aries19920321')
             password.submit()
-            # data_save_windows = WebDriverWait(chrome, 10).until(
-            #     EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/section/div/button'))
-            #  ).click()
-            time.sleep(1)
+            data_save_windows = WebDriverWait(chrome, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/section/div/button'))
+             ).click()
+
             search = WebDriverWait(chrome, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input'))
             )   
