571a111c3c8e3588be1da7a259fc3f49f9a07f68
Bruce <jetliayu@gmail.com>
2021-09-22 12:25:07 +0800

    ok

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ import time
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('CHANNEL_SECRET')
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
