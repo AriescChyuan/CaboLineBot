e36fec2015b1a4f8dcfcbce85a0709daa73b476e
Bruce <jetliayu@gmail.com>
2021-11-17 19:25:30 +0800

    123

--- app.py
+++ app.py
@@ -66,17 +66,17 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=response))
     elif msg.lower().find('#') == 0:
         texturl = parse.quote(msg[1:])
-        header = {      
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
-            'Cookie': 'wluuid=66;  ',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'Accept-encoding': 'gzip, deflate, br',
-            'Accept-language': 'zh-CN,zh;q=0.9',
-            'Cache-Control': 'max-age=0',
-            'connection': 'keep-alive'
-            , 'Host': 'stock.tuchong.com',
-            'Upgrade-Insecure-Requests': '1'
-            }
+        # header = {      
+        #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+        #     # 'Cookie': 'wluuid=66;  ',
+        #     # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        #     # 'Accept-encoding': 'gzip, deflate, br',
+        #     # 'Accept-language': 'zh-CN,zh;q=0.9',
+        #     # 'Cache-Control': 'max-age=0',
+        #     # 'connection': 'keep-alive'
+        #     # , 'Host': 'stock.tuchong.com',
+        #     # 'Upgrade-Insecure-Requests': '1'
+        #     }
         url="https://stock.tuchong.com/search?term={}".format(texturl)
         req=requests.get(url)
         soup=BeautifulSoup(req.text,'html.parser')
@@ -90,9 +90,8 @@ def handle_message(event):
         for i in range(len(va)):
             url = 'https://weiliicimg9.pstatp.com/weili/l/'+va[i][1].strip('\"')+'.webp'
             url_list.append(url)
-        # url = random.choice(url_list[0:100])
-        print("url_list = ",url_list)
-        url = url_list[random.randint(0,len(url_list)-1)]
+        url = random.choice(url_list)
+        # url = url_list[random.randint(0,len(url_list)-1)]
         line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=url, preview_image_url=url))
     # elif msg.lower().find('#') == 0:
     #     # print('driver_path : ',os.environ.get("CHROMEDRIVER_PATH"))
