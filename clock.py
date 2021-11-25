7ce6123c4f4d4e713acd78d49a3867f7a5e74ef8
Bruce <jetliayu@gmail.com>
2021-11-25 11:54:06 +0800

    123

--- clock.py
+++ clock.py
@@ -12,7 +12,7 @@ from linebot.exceptions import LineBotApiError
 from config import *
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-line_bot_userid = USERID
+
 
 sched = BlockingScheduler()
 
@@ -22,10 +22,10 @@ def scheduled_job():
     url = "https://linebot-bruce.herokuapp.com/"
     connect = urllib.request.urlopen(url)
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='11',minute='52')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='11',minute='55')
 def scheduled_job():
     try:
-        line_bot_api.push_message('U24c11ad61979778f76d11fb91ce15bf8', TextSendMessage(text='早安！！'))
+        line_bot_api.push_message(USERID, TextSendMessage(text='測試'))
     except LineBotApiError as e:
         print('MessagePush Error:',e)
 
