3333b80d7d0fbf41e65e39a62b3fa54df90010fc
Bruce <jetliayu@gmail.com>
2021-11-25 11:28:41 +0800

    123

--- clock.py
+++ clock.py
@@ -13,8 +13,7 @@ from config import *
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 line_bot_userid = USERID
-# sched = BackgroundScheduler()
-# scheduler2 = BackgroundScheduler()
+
 sched = BlockingScheduler()
 
 # 定義排程 : 在周一至周五，每 20 分鐘就做一次 def scheduled_jog()
@@ -23,15 +22,15 @@ def scheduled_job():
     url = "https://linebot-bruce.herokuapp.com/"
     connect = urllib.request.urlopen(url)
 
-# @sched.scheduled_job('cron', day_of_week='mon-sun', hour='11',minute='5')
-# def scheduled_job():
-#     try:
-#         text = '早安！！'
-#         line_bot_api.push_message(line_bot_userid, TextSendMessage(text=text))
-#     except LineBotApiError as e:
-#         print('MessagePush Error:',e)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='11',minute='30')
+def scheduled_job():
+    try:
+        text = '早安！！'
+        line_bot_api.push_message(line_bot_userid, TextSendMessage(text=text))
+    except LineBotApiError as e:
+        print('MessagePush Error:',e)
 
-# scheduler1.start()  
+ 
 sched.start()  
 
 
