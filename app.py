7dffcf04b37017a5a282b42a16beaaf0a0f0ac61
Bruce <jetliayu@gmail.com>
2021-11-26 09:35:22 +0800

    123

--- app.py
+++ app.py
@@ -131,14 +131,13 @@ def handle_message(event):
         depth = results[0].find_all('li')[1].text[2:]
         scale = results[0].find_all('li')[2].text[4:]
         url  = 'https://www.cwb.gov.tw/' + results[0].get('href')
-        string = ''' 
-        最近一次地震：
-        時間：{}
-        地點：{}
-        深度：{}
-        規模：{}
-        點我看更多：{}
-        '''.format(time, location, depth, scale, url)
+        string = ''' 最近一次地震：
+                     時間：{}
+                     地點：{}
+                     深度：{}
+                     規模：{}
+                     點我看更多：{}
+                '''.format(time, location, depth, scale, url)
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=string))
     else:       
         if ans != '':    
