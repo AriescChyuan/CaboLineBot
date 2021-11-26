fb9f6df85614f8662ed3b23152b8aba890bad11c
Bruce <jetliayu@gmail.com>
2021-11-26 09:32:29 +0800

    123

--- app.py
+++ app.py
@@ -131,7 +131,8 @@ def handle_message(event):
         depth = results[0].find_all('li')[1].text[2:]
         scale = results[0].find_all('li')[2].text[4:]
         url  = 'https://www.cwb.gov.tw/' + results[0].get('href')
-        string = ''' 最近一次地震：\n
+        string = ''' 
+        最近一次地震：
         時間：{}
         地點：{}
         深度：{}
