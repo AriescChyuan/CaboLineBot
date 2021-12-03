30de446e28d329fad410ae7d8c14e1b7fc1ab1ca
Bruce <jetliayu@gmail.com>
2021-12-03 21:30:02 +0800

    123

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -1,8 +1,9 @@
-import paho.mqtt.client as mqtt
-# import random
+import time
+import paho.mqtt.client as paho
+from paho import mqtt
 import json  
 import datetime 
-import time
+
 
 def fan_control():
     # 設置日期時間的格式
@@ -10,13 +11,14 @@ def fan_control():
 
     # 連線設定
     # 初始化地端程式
-    client = mqtt.Client()
+    client = paho.Client(client_id="linebot", userdata=None, protocol=paho.MQTTv5)
 
     # 設定登入帳號密碼
     client.username_pw_set("Bruce","Aries19920321")
 
     # 設定連線資訊(IP, Port, 連線時間)
     client.connect("337d8bc43b214494bf2990e1f5d1e905.s2.eu.hivemq.cloud", 8883, 60)
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 
     while True:
         # t0 = random.randint(0,30)
