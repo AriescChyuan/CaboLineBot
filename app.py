56a2aa514e0421cb18356d2a63d6efed0edc4ab9
Bruce <jetliayu@gmail.com>
2022-01-14 16:12:35 +0800

    123

--- app.py
+++ app.py
@@ -115,6 +115,9 @@ def handle_message(event):
         dice = ['1','2','3','4','5','6']
         ans = "您擲到的骰子點數為：{}點。".format(random.choice(dice))
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ans))
+    elif ans == 'ELRS':
+        url = 'https://github.com/ExpressLRS/ExpressLRS'
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=url))
     elif ans == '晚餐':
         ls = ['麥當勞','雞滷飯','火鍋','炒飯','泡麵','水果吃到飽','鍋貼','滷味']
         ans = random.choice(ls)
