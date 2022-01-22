71a7ccdd5db252ee9dc70dd23a56bf0efdb1aeac
Bruce <jetliayu@gmail.com>
2022-01-22 16:19:57 +0800

    123

--- app.py
+++ app.py
@@ -230,7 +230,9 @@ def handle_message(event):
             longitude=120.6056722
         )
         line_bot_api.reply_message(event.reply_token, location_message)
-    
+    elif ans == 'test':
+        message = carousel_template()
+        line_bot_api.reply_message(event.reply_token, message)
     else:      
         pass
         # if ans != '':    
