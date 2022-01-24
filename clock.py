d97cc1abe1ac0f3a533f5d2c7cae42c456d4275e
Bruce <jetliayu@gmail.com>
2022-01-25 00:02:08 +0800

    123

--- clock.py
+++ clock.py
@@ -23,7 +23,7 @@ def scheduled_job():
     url = "https://linebot-bruce.herokuapp.com/"
     connect = urllib.request.urlopen(url)
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='23',minute='45')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='24',minute='5')
 def scheduled_job():
     fan_control("1")
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour='7',minute='30')
