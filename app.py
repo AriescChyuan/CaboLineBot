e4123d5dfa42268a502b24a6f3ed0f58febfc212
Bruce <jetliayu@gmail.com>
2022-05-12 17:23:39 +0800

    123

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def handle_message(event):
     # elif greeting_resp != None:
     #     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=greeting_resp))
 
-    if ans == '正妹' or ans =='beauty':
+    if ans == '正妹' or ans.lower() =='beauty':
 
         # headers = {
         # 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
