4d38c7a704a906ba0665f83b738df2656e9ae772
AriesChyuan <jetliayu@gmail.com>
2023-05-18 20:13:08 +0800

    update

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    json_data = json.loads(body)
+    
     # handle webhook body
     try:
         handler.handle(body, signature)
