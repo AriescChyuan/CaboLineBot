3f5e101f83be269c024b3decd2a0a87fd5ca1172
AriesChyuan <jetliayu@gmail.com>
2023-08-03 08:51:25 +0800

    test

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ app = Flask(__name__)
 # line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 # handler = WebhookHandler(CHANNEL_SECRET)
 
-configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+line_bot_api = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
