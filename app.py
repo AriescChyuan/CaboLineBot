58540b93a4b294e8c29d746d33cb72d91ecda63d
AriesChyuan <jetliayu@gmail.com>
2023-08-03 12:48:47 +0800

    1223

--- app.py
+++ app.py
@@ -1,119 +1,31 @@
-import datetime
-# import errno
-import os
-import sys
-import logging
-# import tempfile
-from argparse import ArgumentParser
+from flask import Flask, request, abort
 
-from flask import Flask, request, abort, send_from_directory
-from werkzeug.middleware.proxy_fix import ProxyFix
-
-# from linebot.v3 import (
-#     WebhookHandler
-# )
-from linebot.v3.webhook import WebhookHandler
-from linebot.v3.models import (
-    UnknownEvent
+from linebot.v3 import (
+    WebhookHandler
 )
 from linebot.v3.exceptions import (
     InvalidSignatureError
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent,
-    LocationMessageContent,
-    StickerMessageContent,
-    ImageMessageContent,
-    VideoMessageContent,
-    AudioMessageContent,
-    FileMessageContent,
-    UserSource,
-    RoomSource,
-    GroupSource,
-    FollowEvent,
-    UnfollowEvent,
-    JoinEvent,
-    LeaveEvent,
-    PostbackEvent,
-    BeaconEvent,
-    MemberJoinedEvent,
-    MemberLeftEvent,
 )
 from linebot.v3.messaging import (
     Configuration,
     ApiClient,
     MessagingApi,
-    MessagingApiBlob,
     ReplyMessageRequest,
-    PushMessageRequest,
-    MulticastRequest,
-    BroadcastRequest,
-    TextMessage,
-    ApiException,
-    LocationMessage,
-    StickerMessage,
-    ImageMessage,
-    TemplateMessage,
-    FlexMessage,
-    Emoji,
-    QuickReply,
-    QuickReplyItem,
-    ConfirmTemplate,
-    ButtonsTemplate,
-    CarouselTemplate,
-    CarouselColumn,
-    ImageCarouselTemplate,
-    ImageCarouselColumn,
-    FlexBubble,
-    FlexImage,
-    FlexBox,
-    FlexText,
-    FlexIcon,
-    FlexButton,
-    FlexSeparator,
-    FlexContainer,
-    MessageAction,
-    URIAction,
-    PostbackAction,
-    DatetimePickerAction,
-    CameraAction,
-    CameraRollAction,
-    LocationAction,
+    TextMessage
 )
-
-from linebot.v3.insight import (
-    ApiClient as InsightClient,
-    Insight
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
 )
-
-from Function import *
-from random_speak import  *
 import os
-import requests
-import re
-from bs4 import BeautifulSoup
-import random
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_proto=1)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-app.logger.setLevel(logging.INFO)
-
-
-# get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('CHANNEL_SECRET', None)
 channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None or channel_access_token is None:
-    print('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
-    sys.exit(1)
 
+configuration = Configuration(access_token=channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-
-configuration = Configuration(
-    access_token=channel_access_token
-)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -145,39 +57,5 @@ def handle_message(event):
             )
         )
 
-
-        # elif text == '正妹' or text.lower() =='beauty' or text == '抽' or text == "美女" or text == "振銓前女友":
-
-        #     response = requests.get('https://www.jkforum.net/type-736-1940.html?forumdisplay&typeid=1940&filter=typeid&typeid=1940&forumdisplay=&page=1')
-        #     soup = BeautifulSoup(response.text, 'html.parser')
-        #     # max_page = soup.find_all('label')[0].find('span').string.split(' ')[2]
-        #     max_page = int(soup.find_all(title=re.compile('共\s\d+\s頁'))[0].string.split(' ')[2])
-        #     random_page = random.randint(1, int(max_page)-2)
-
-        #     # url = f"https://www.jkforum.net/forum-736-{random_page}.html"
-        #     url = f"https://www.jkforum.net/type-736-1940.html?forumdisplay&typeid=1940&filter=typeid&typeid=1940&forumdisplay=&page={random_page}"
-        #     res = requests.get(url)
-        #     soup = BeautifulSoup(res.text, 'html.parser')
-        #     images = soup.find_all("img", src=re.compile("https://www.my"))[:-3]
-        #     images_len = len(images)
-        #     image_url = soup.find_all("img", src=re.compile("https://www.my"))[:-3][random.randint(0, images_len-1)].get("src")
-
-        #     line_bot_api.reply_message(
-        #     ReplyMessageRequest(
-        #         reply_token=event.reply_token,
-        #         messages=[
-        #             ImageMessage(original_content_url=image_url, preview_image_url=image_url)
-        #         ]
-        #     )
-        # )
 if __name__ == "__main__":
-    # arg_parser = ArgumentParser(
-    #     usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    # )
-    # arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    # arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    # options = arg_parser.parse_args()
-
-    # app.run(debug=options.debug, port=options.port)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
